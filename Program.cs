ce21673dc6e8f0edadd8f73e9727f4a97f104c9a
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-14 19:01:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using Demo_Assemblies;
+
+namespace Demo_Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Assemblies in csharp!");
+            Stringer myStringInstance = new Stringer();
+            Console.WriteLine("client code excecutes");
+            myStringInstance.stringermethod();
+        }
+    }
+}
---
+++ Stringer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_Assemblies
+{
+    class Stringer
+    {
+        public void stringermethod()
+        {
+            Console.WriteLine("This method is defined in demoassemblies nampspace under class stringer");
+
+        }
+    }
+}
